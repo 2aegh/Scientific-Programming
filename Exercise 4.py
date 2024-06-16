4bf1c0a79b58d6758ef2bf404d0066d4eba4eb82
Amirali Eghtesad <107042246+2aegh@users.noreply.github.com>
2024-06-16 23:09:51 +0330

    Create Exercise 4.py

---
+++ Exercise 4.py
@@ -0,0 +1,113 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+from IPython.display import clear_output, display
+import time
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(784, 256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        x = x.view(x.size(0), 784)
+        output = self.model(x)
+        return output
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(100, 256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 784),
+            nn.Tanh(),
+        )
+
+    def forward(self, x):
+        output = self.model(x)
+        output = output.view(x.size(0), 1, 28, 28)
+        return output
+
+def train_gan(batch_size: int = 32, num_epochs: int = 100, device: str = "cuda:0" if torch.cuda.is_available() else "cpu"):
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+
+    try:
+        train_set = torchvision.datasets.MNIST(root=".", train=True, download=True, transform=transform)
+    except:
+        print("Failed to download MNIST, retrying with different URL")
+        torchvision.datasets.MNIST.resources = [
+            ('https://ossci-datasets.s3.amazonaws.com/mnist/train-images-idx3-ubyte.gz',
+             'f68b3c2dcbeaaa9fbdd348bbdeb94873'),
+            ('https://ossci-datasets.s3.amazonaws.com/mnist/train-labels-idx1-ubyte.gz',
+             'd53e105ee54ea40749a09fcbcd1e9432'),
+            ('https://ossci-datasets.s3.amazonaws.com/mnist/t10k-images-idx3-ubyte.gz',
+             '9fb629c4189551a2d022fa330f9573f3'),
+            ('https://ossci-datasets.s3.amazonaws.com/mnist/t10k-labels-idx1-ubyte.gz',
+             'ec29112dd5afa0611ce80d1b7f02629c')
+        ]
+        train_set = torchvision.datasets.MNIST(root=".", train=True, download=True, transform=transform)
+
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
+
+    discriminator = Discriminator().to(device)
+    generator = Generator().to(device)
+    lr = 0.0001
+    loss_function = nn.BCELoss()
+    optimizer_discriminator = torch.optim.Adam(discriminator.parameters(), lr=lr)
+    optimizer_generator = torch.optim.Adam(generator.parameters(), lr=lr)
+
+    for epoch in range(num_epochs):
+        for n, (real_samples, mnist_labels) in enumerate(train_loader):
+
+            real_samples = real_samples.to(device=device)
+            current_batch_size = real_samples.size(0)
+            real_samples_labels = torch.ones((current_batch_size, 1)).to(device=device)
+            latent_space_samples = torch.randn((current_batch_size, 100)).to(device=device)
+            generated_samples = generator(latent_space_samples)
+            generated_samples_labels = torch.zeros((current_batch_size, 1)).to(device=device)
+            all_samples = torch.cat((real_samples, generated_samples))
+            all_samples_labels = torch.cat((real_samples_labels, generated_samples_labels))
+
+            discriminator.zero_grad()
+            output_discriminator = discriminator(all_samples)
+            loss_discriminator = loss_function(output_discriminator, all_samples_labels)
+            loss_discriminator.backward()
+            optimizer_discriminator.step()
+
+            latent_space_samples = torch.randn((current_batch_size, 100)).to(device=device)
+
+            generator.zero_grad()
+            generated_samples = generator(latent_space_samples)
+            output_discriminator_generated = discriminator(generated_samples)
+            loss_generator = loss_function(output_discriminator_generated, real_samples_labels)
+            loss_generator.backward()
+            optimizer_generator.step()
+
+            if n == len(train_loader) - 1:
+                name = f"Epoch: {epoch}\nLoss D.: {loss_discriminator:.2f} Loss G.: {loss_generator:.2f}"
+                generated_samples = generated_samples.detach().cpu().numpy()
+                fig = plt.figure()
+                for i in range(16):
+                    sub = fig.add_subplot(4, 4, 1 + i)
+                    sub.imshow(generated_samples[i].reshape(28, 28), cmap="gray_r")
+                    sub.axis('off')
+                fig.suptitle(name)
+                fig.tight_layout()
+                clear_output(wait=False)
+                display(fig)
+
+# Test the function
+train_gan(batch_size=64, num_epochs=100)
